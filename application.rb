1d22847d5ae914cfb8dcb1505f2bee26fd5c4ab1
Ryan Hamilton <ryanahamilton@gmail.com>
2012-03-05 13:07:53 -0500

    Adds Pony gem for contact mailing, new views for individual projects, modernizer script

--- application.rb
+++ application.rb
@@ -1,6 +1,15 @@
 # application.rb
 require 'sinatra'
-require 'sass'
+require 'sass' # CSS Preprocessing
+require 'pony' # Email Delivery
+
+# Variables
+set :app_domain, ENV['APP_DOMAIN'] || 'localhost:4567'
+
+# SendGrid Variables
+set :email_to, 'ryanahamilton@gmail.com'
+set :email_username, ENV['SENDGRID_USERNAME'] || 'username'
+set :email_password, ENV['SENDGRID_PASSWORD'] || 'secret'
 
 # SCSS Stylesheet
 get '/stylesheets/main.css' do
@@ -13,7 +22,26 @@ get '/' do
   erb :index, :layout => :layout
 end
 
-# Work Modal
-get '/work' do
-  erb :work, :layout => false
+# Contact Form Submission
+post '/contact' do
+  Pony.mail :to => settings.email_to,
+            :from => params[:name] + "<" + params[:email] + ">",
+            :subject => "Message from #{settings.app_domain}",
+            :body => params[:message],
+            :via => :smtp,
+            :via_options => {
+              :address              => 'smtp.sendgrid.net',
+              :port                 => '587',
+              :enable_starttls_auto => true,
+              :user_name            => settings.email_username,
+              :password             => settings.email_password,
+              :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+              :domain               =>  settings.app_domain # the HELO domain provided by the client to the server
+            }
+  redirect '/'
+end
+
+# Work Modal Views
+get '/work/:name' do |n|
+  erb :"work/#{n}", :layout => false
 end
